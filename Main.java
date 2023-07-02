cdb4ae4d1824a91cfb3fd1f564a0ef3c9eae2b02
oruccakir <oruccakir2525@gmail.com>
2023-07-02 15:04:49 +0300

    main is arranged

--- Main.java
+++ Main.java
@@ -2,17 +2,10 @@ public class Main {
     
     public static void main(String[] args) {
         
-        MaxHeap heap = new MaxHeap(10);
-        MinHeap heap2 = new MinHeap(10);
+        MinHeap<Integer> heap2 = new MinHeap<>(10);
 
-        for(int i=9; i>=0; i--) heap.insertKey(i);
         for(int i=0; i<10; i++) heap2.insertKey(i);
-
-        heap.deleteKey(7);
-        heap.extractMax();
-        heap.extractMax();
-
-        System.out.println(heap);
+        
         System.out.println(heap2);
 
 
