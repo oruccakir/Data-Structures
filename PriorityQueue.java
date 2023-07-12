54b4519c8dbd6605d3c270fbcc7e3af9446d96b3
oruccakir <oruccakir2525@gmail.com>
2023-07-12 20:29:00 +0300

    height and print method were added

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+
 public class PriorityQueue<T extends Comparable<T>> implements PriorityInterface<T> {
 
     private MaxHeap<T> maxHeap;
@@ -213,6 +215,100 @@ public class PriorityQueue<T extends Comparable<T>> implements PriorityInterface
         return (T[])arr;
     }
 
-    
+    public int height(int index){
+
+        int left = 2*index+1;
+        int right = 2*index+2;
+        
+        if(type.equals("Max")){
+
+            
+            
+            if((left < maxHeap.heap_size && maxHeap.heap_array[left] == null || left >= maxHeap.heap_size ) && (right < maxHeap.heap_size && maxHeap.heap_array[right] == null || right >= maxHeap.heap_size )  )
+               return 0;
+
+            return 1 + Math.max(height(left), height(right));
+
+
+        }
+        else{
+
+
+            if((left < minHeap.heap_size && minHeap.heap_array[left] == null || left >= minHeap.heap_size ) && (right < minHeap.heap_size && minHeap.heap_array[right] == null || right >= minHeap.heap_size )  )
+               return 0;
+
+            return 1 + Math.max(height(left), height(right));
+
+
+        }
+
+
+    }
+
+
+    public void print_as_tree(){
+
+        if(type.equals("Max") && maxHeap.heap_size == 0) return;
+
+        else if(type.equals("Min")  && minHeap.heap_size == 0) return;
+
+        Object temp_heap[] = null;
+
+        int height = 0;
+
+        int heap_size = 0;
+
+        if(type.equals("Max")){
+            temp_heap = maxHeap.heap_array;
+            heap_size = maxHeap.heap_size;
+        } 
+
+        else {
+            temp_heap = minHeap.heap_array;
+            heap_size = minHeap.heap_size;
+        }    
+
+        ArrayDeque<Object> dq = new ArrayDeque<>();
+
+        dq.addLast(temp_heap[0]);
+
+        height = height(0);
+
+        int index = 0;
+
+        int left = 2*index+1;
+        int right = 2*index+2;
+
+        while(dq.isEmpty() == false){
+
+            int size = dq.size();
+
+            for(int i=0; i<height; i++) System.out.print("      ");
+
+            for(int i=0; i<size; i++){
+
+                Object data = dq.pollFirst();
+
+                System.out.print(data+"     ");
+
+                left = 2*index+1;
+
+                right = 2*index+2;
+
+                if(left < heap_size) dq.addLast(temp_heap[left]);
+
+                if(right < heap_size) dq.addLast(temp_heap[right]);
+
+                index++;
+
+            }
+
+            height--;
+
+            System.out.println();
+
+        }
+
+    } 
     
 }
