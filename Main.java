bf164df91b73947c053ba461679c2fa51073ec86
oruccakir <oruccakir2525@gmail.com>
2023-08-25 22:31:35 +0300

    testing

--- Main.java
+++ Main.java
@@ -1,36 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     
     public static void main(String[] args) {
         
-        int k = 6;
+        int arr[] = {4,1,3,2,7,9,10,14,8,16};
 
-        Integer arr[] = {1,5,9,6,1,21,12,15,14};
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>("Min",arr.length);
-
-        for(int i=0; i<arr.length; i++){
-
-                pq.add(arr[i]);
-        }
-    
-        System.out.println();
-
-        pq.pop();
-        pq.add(0);
-        pq.pop();
-        pq.pop();
-        pq.pop();
-        pq.pop();
-        pq.pop();
-        pq.pop();
-        pq.pop();
-        pq.pop();
-        pq.pop();
-
-        pq.print_as_tree();
-
+        Heap.heapSortIncrease(arr, arr.length);
        
-
+        System.out.println(Arrays.toString(arr));
        
        
        
